a85a725e1f069ba38f877b4faaa39d63d3622edf
Neves-Rafael <nevesrafael.dev@gmail.com>
2023-10-08 13:15:02 -0300

    envio base

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className="text-red-500">teste</p>
+    </>
+  );
+}
+
+export default App;
