36456dafd5515aae34a92bb40b9615ec228b7b52
Neves-Rafael <nevesrafael.dev@gmail.com>
2023-10-22 23:31:32 -0300

    tentativa para import

--- src/scenes/components/today.tsx
+++ src/scenes/components/today.tsx
@@ -1,17 +1,128 @@
-// import { useEffect, useState } from "react";
 import { useState } from "react";
-// import { day1 } from "@/assets/index";
+
+import {
+  day1,
+  day2,
+  day3,
+  day4,
+  day5,
+  day6,
+  day7,
+  day8,
+  day9,
+  day10,
+  night1,
+  night2,
+  night3,
+  night4,
+  night5,
+  night6,
+  night7,
+  night8,
+  night9,
+  night10,
+  night50,
+} from "@/assets/index";
 export function Today(props: {
   dados: any;
   inputValue: string;
   imagem: string;
 }) {
-  const [icons, setIcons] = useState("day1");
-  const tentativaImport = async () => {
-    const icons = await import("@/assets/01d.svg");
-    setIcons(icons.default);
-    console.log(tentativaImport);
+  const icons = props.imagem;
+
+  // switch (icons) {
+  //   case "01d":
+  //     setImagem(day1);
+  //     break;
+  //   case "02d":
+  //     setImagem(day2);
+  //     break;
+  //   case "03d":
+  //     setImagem(day3);
+  //     break;
+  //   case "04d":
+  //     setImagem(day4);
+  //     break;
+  //   case "05d":
+  //     setImagem(day5);
+  //     break;
+  //   case "06d":
+  //     setImagem(day6);
+  //     break;
+  //   case "07d":
+  //     setImagem(day7);
+  //     break;
+  //   case "08d":
+  //     setImagem(day8);
+  //     break;
+  //   case "09d":
+  //     setImagem(day9);
+  //     break;
+  //   case "10d":
+  //     setImagem(day10);
+  //     break;
+  //   case "01n":
+  //     setImagem(night1);
+  //     break;
+  //   case "02n":
+  //     setImagem(night2);
+  //     break;
+  //   case "03n":
+  //     setImagem(night3);
+  //     break;
+  //   case "04n":
+  //     setImagem(night4);
+  //     break;
+  //   case "05n":
+  //     setImagem(night5);
+  //     break;
+  //   case "06n":
+  //     setImagem(night6);
+  //     break;
+  //   case "07n":
+  //     setImagem(night7);
+  //     break;
+  //   case "08n":
+  //     setImagem(night8);
+  //     break;
+  //   case "09n":
+  //     setImagem(night9);
+  //     break;
+  //   case "10n":
+  //     setImagem(night10);
+  //     break;
+  //   case "50n":
+  //     setImagem(night50);
+  //     break;
+  // }
+
+  const iconsConfig = {
+    "01d": day1,
+    "02d": day2,
+    "03d": day3,
+    "04d": day4,
+    "05d": day5,
+    "06d": day6,
+    "07d": day7,
+    "08d": day8,
+    "09d": day9,
+    "10d": day10,
+    "01n": night1,
+    "02n": night2,
+    "03n": night3,
+    "04n": night4,
+    "05n": night5,
+    "06n": night6,
+    "07n": night7,
+    "08n": night8,
+    "09n": night9,
+    "10n": night10,
+    "50n": night50,
   };
+
+  const imagem = iconsConfig[icons];
+
+  console.log(imagem);
 
   return (
     <div className="w-[500px] h-[250px] bg-slate-200 rounded-lg text-black flex justify-between flex-col">
@@ -29,7 +140,7 @@ export function Today(props: {
           </div>
         </div>
         <div className="flex justify-between items-center">
-          <img src={icons} className="w-[90px] h-[90px]" />
+          <img src={imagem} className="w-[90px] h-[90px]" />
           <div>
             <p className="text-7xl text-black font-black text-center">
               {props.dados.main.temp}°C
